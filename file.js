121771b2122bb0c4dcccab105c66533d8d231728
Eduardo Kawassaki <eduardok.fx@gmail.com>
2016-12-20 21:32:57 -0300

    node

--- file.js
+++ file.js
@@ -1,9 +1,44 @@
-//fs = file system
 var fs = require('fs');
 
-
+/*
+// Cria arquivo
 fs.writeFile('data.txt', "Eduardo Kawassaki",function(err){
     if(err){
         throw err;
     }
 });
+*/
+
+/*
+//ler arquivo
+fs.readFile('data.txt', function(err, data){
+    if(err){
+        throw err;
+    }
+
+    console.log(data.toString('utf8'));
+});
+*/
+
+/*
+//lê arquivos
+fs.readdir('.',function(err,files){
+    if(err){
+        throw err;
+    }
+
+    for(var file in files){
+        console.log(files[file]);
+    }
+});
+*/
+
+//retorna somente arquivos js
+fs
+    .readdirSync('.')
+    .filter(function(file) {
+        return (file.endsWith('.js'));
+    })
+    .forEach(function(file){
+        console.log(file);
+    })
